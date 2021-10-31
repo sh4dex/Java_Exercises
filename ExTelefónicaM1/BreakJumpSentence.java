634cce1017b3fc4ede36ca9bc7c6bffd01ac14e5
Thomas <thomas@sorza.com>
2021-10-31 12:10:21 -0500

    [

---
+++ ExTelefónicaM1/BreakJumpSentence.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class BreakJumpSentence{
+    public static void main(String[] args) {
+        /*El programa continua su ejecución hasta
+        que de forma aleatoria se genere el número cero
+        para las variables num1 o num2*/
+        Random aleatorio = new Random();
+        int num1 =0, num2=0;
+        for( ; ; ){
+            num1= aleatorio.nextInt(50);
+                if (num1 == 0) break;
+            num2= aleatorio.nextInt(1000);
+                if (num2 == 0) break;
+            System.out.print("num2: "+ num2 + " num1: " + num1 );
+            System.out.print(" num2/num1 = " + num2 / num1);
+            System.out.println(" num2%num1 = " + num2 % num1);
+        }
+    }
+}

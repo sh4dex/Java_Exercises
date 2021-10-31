22d959ca994dd34a5b0e683d02d3276d3bf5a936
Thomas <thomas@sorza.com>
2021-10-31 11:56:39 -0500

    [add] file ExampleContinueSentence Exercise with continue

---
+++ ExTelefónicaM1/ExampleContinueSentence.java
@@ -0,0 +1,12 @@
+public class ExampleContinueSentence{
+    public static void main(String args []) {
+        /*Ejercicio para determinar como funciona
+        la sentencia continue*/
+        int i = 0;
+        while(i++<10){
+        if (++i%3!=0) continue;
+        System.out.print ("El numero es "+i);
+        //Output: El número es 6
+        }
+    }
+}

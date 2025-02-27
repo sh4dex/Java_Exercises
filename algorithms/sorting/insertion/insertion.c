cceae4bac33fb5cb4e31a31f173c0e0d435c4d39
Thomas Sorza <sthx09567541@gmail.com>
2025-02-27 13:39:20 -0500

    Finished Insertion Sort in C

--- algorithms/sorting/insertion/insertion.c
+++ algorithms/sorting/insertion/insertion.c
@@ -3,17 +3,29 @@
 void insertion_sort(int arr[], int length);
 
 int main(){
-    int a[] = {3,2,1,5,4};
+    int a[] = {-3,2,-1,54,0,200,1,-100};
+    printf("Array to sort:\n");
     int length = sizeof(a) / 4;
+    for(int i=0; i<length; i++){
+      printf("%d\t", a[i]);
+    }
+    printf("\n");
     int temp;
 
-    for (int i = 1; i<length; i+=){
+    for (int i = 1; i<length; i++){
       int j = i;
-      while( j>0 && a[j-1] < a[j]){
-        //TODO: Finish algo
+      //while( j>0 && a[j-1] < a[j]){ for increasing order
+      while( j>0 && a[j]< a[j-1]){
+        temp=a[j-1];
+        a[j-1]=a[j];
+        a[j]=temp;
+        j--;
       }
-      j--;
     }
-    printf("%d\n", length);
+
+    printf("Sorted Array:\n");
+    for(int i=0; i<length; i++){
+      printf("%d\t", a[i]);
+    }
     return 0;
 }

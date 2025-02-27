393aacd293d4cbc6e37899dda68a70b22251abcc
Thomas Sorza <sthx09567541@gmail.com>
2025-02-27 15:43:41 -0500

    Organized propperly insertion.c

--- algorithms/sorting/insertion/insertion.c
+++ algorithms/sorting/insertion/insertion.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
-void insertion_sort(int arr[], int length);
 
-int main(){
-    int a[] = {-3,2,-1,54,0,200,1,-100};
-    printf("Array to sort:\n");
-    int length = sizeof(a) / 4;
-    for(int i=0; i<length; i++){
-      printf("%d\t", a[i]);
-    }
-    printf("\n");
-    int temp;
+int a[] = {-3,2,-1,54,0,200,1,-100};
+int length = sizeof(a) / 4;
 
-    for (int i = 1; i<length; i++){
+void insertion_sort(int a[]){
+  int temp;
+  for (int i = 1; i<length; i++){
       int j = i;
       //while( j>0 && a[j-1] < a[j]){ for increasing order
       while( j>0 && a[j]< a[j-1]){
@@ -22,10 +16,20 @@ int main(){
         j--;
       }
     }
+}
 
+void printArrln(int arr[]){
+  for(int i=0; i<length; i++){
+    printf("%d\t", a[i]);
+  }
+  printf("\n");
+}
+
+int main(){
+    printf("Array to sort:\n");
+    printArrln(a);
+    insertion_sort(a);
     printf("Sorted Array:\n");
-    for(int i=0; i<length; i++){
-      printf("%d\t", a[i]);
-    }
+    printArrln(a); 
     return 0;
 }
